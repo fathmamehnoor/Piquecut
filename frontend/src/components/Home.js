39424887449a6698bbe42b95d6232ce0f53ecd32
fathmamehnoor <fathmamehnoor@example.com>
2024-06-17 05:00:09 +0530

    changes to frontend

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -72,7 +72,7 @@ const Home = () => {
             }));
     
             // Send POST request to predict endpoint
-            const response = await axios.post('piquecut-backend.azurewebsites.net/predict', { observation: dataset });
+            const response = await axios.post('https://piquecut-backend.azurewebsites.net/predict', { observation: dataset });
     
             // Assuming the response contains image_path
             setLayoutImageURL(response.data.image_path);
